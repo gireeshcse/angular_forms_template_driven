57ffc355f0b2af68ec4b2458e0346bc5dd7271d7
gireesh <gireeshcse@live.com>
2019-05-04 00:12:00 +0530

    form validation added

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
-
+  userSettings:UserSettings={
+    name:null,
+    emailOffers:null,
+    interfaceStyle:null,
+    subscriptionType:null,
+    notes: null
+  };
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(myForm:NgForm){
+    console.log(myForm.valid);
   }
 
 }
